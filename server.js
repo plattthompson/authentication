ac068fcf67af35d94b43463e1ae2ad1895cf7b6b
ctrlaltdev <yorick@ctrlalt.dev>
2019-10-28 21:35:39 -0700

    Move to mongo

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const db = require('./models/');
 
 const app = express();
 
@@ -9,39 +12,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/authenticate', { useUnifiedTopology: true, useNewUrlParser: true });
 
-app.post('/register', (req, res) => {
-	console.info(req.body);
-	res.json(req.body);
-});
-app.get('/register', (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/register.html'));
-});
-
-app.post('/login', (req, res) => {
-	console.info(req.body);
-	res.json(req.body);
-});
-app.get('/login', (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/login.html'));
-});
-
-app.post('/logout', (req, res) => {
-	res.send(200);
-});
-
-app.get('/secure', (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/secure.html'));
-});
-
-app.post('/', (req, res) => {
-	res.json('Got your request!');
-});
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+require('./routes')(app);
 
 app.listen(process.env.PORT||3000, () => {
 	console.log(`App is running on port ${process.env.PORT}`);
