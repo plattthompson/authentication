d45d72a8a230f7e85b603f1b9184a33cfa8dde69
ctrlaltdev <yorick@ctrlalt.dev>
2019-11-01 20:06:33 -0700

    Fix typo

--- routes/html.js
+++ routes/html.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const router = require('express').Router();
 
-router,route('/register')
+router.route('/register')
   .get((req, res) => {
     res.sendFile(path.join(__dirname, '../public/register.html'));
   })
 
-router,route('/login')
+router.route('/login')
   .get((req, res) => {
     res.sendFile(path.join(__dirname, '../public/login.html'));
   })
 
-router,route('/secure')
+router.route('/secure')
   .get((req, res) => {
     res.sendFile(path.join(__dirname, '../public/secure.html'));
   });
